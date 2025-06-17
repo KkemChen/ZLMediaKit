447483614c748258015a31e895761d53711f0a05
KkemChen <kkemchen@qq.com>
2025-06-17 11:45:12 +0800

    revert: FindFAAC.cmake

--- cmake/FindFAAC.cmake
+++ cmake/FindFAAC.cmake
@@ -1,17 +1,16 @@
-﻿# 查找 include 路径
+﻿
+
 find_path(FAAC_INCLUDE_DIR
   NAMES faac.h
-  PATHS "D:\\kkem\\test\\faac\\include"
 )
 
-# 查找库文件
 find_library(FAAC_LIBRARY
   NAMES faac
-  PATHS "D:\\kkem\\test\\faac\\lib"
 )
 
 set(FAAC_INCLUDE_DIRS ${FAAC_INCLUDE_DIR})
 set(FAAC_LIBRARIES ${FAAC_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
+
 find_package_handle_standard_args(FAAC DEFAULT_MSG FAAC_LIBRARY FAAC_INCLUDE_DIR)
